92cb21a484772c1b09b1da94bfef30c941dc5770
Amit Prajapati <amitprajapati0285@gmail.com>
2026-01-14 18:14:40 +0530

    Update in Cravings for login & register

--- Cravings/server/index.js
+++ Cravings/server/index.js
@@ -2,24 +2,33 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
-import cors from 'cors';
+import cors from "cors";
+import morgan from "morgan";
 import connectDB from "./src/config/db.js";
+import AuthRouter from "./src/routers/authRouterf.js";
 
 const app = express();
 
-app.use(cors({ origin:"http//localhost/5173"}));
+app.use(cors({ origin: "http://localhost:5173" }));
 app.use(express.json());
 
-// app.use("/auth", AuthRouter);
+app.use(morgan("dev"));
+
+app.use("/auth", AuthRouter);
 
 app.get("/", (req, res) => {
   console.log("Server is Working");
 });
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-    console.log("Server Started  at port:",port); 
-    connectDB();   
+app.use((err, req, res, next) => {
+  const Error = err.message || "Internal Server Error";
+  const StausCode = err.statusCode || 500;
+
+  res.status(StausCode).json({ message: ErrorMessage });
 });
 
-
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log("Server Started  at port:", port);
+  connectDB();
+});
---
+++ Cravings/server/src/controllers/authcontroller.js
@@ -0,0 +1,83 @@
+import User from "../models/userModel.js";
+import bcrypt from "bcrypt";
+
+export const UserRegister = async (req, res, next) => {
+  try {
+    
+    const { fullName, email, mobileNumber, password } = req.body;
+
+    if (!fullName || !email || !mobileNumber || !password) {
+      const error = new Error("All feilds required");
+      error.statusCode = 400;
+      return next(error);
+    }
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      const error = new Error("Email already registered");
+      error.statusCode = 409;
+      return next(error);
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(password, salt);
+
+    //save data to database
+
+    const newUser = await User.create({
+      fullName,
+      email,
+      mobileNumber,
+      password: hashPassword,
+    });
+
+    //
+    console.log(newUser);
+
+    res.status(201).json({ Message: "Registration Successfull" });
+    //End
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const UserLogin = async (req, res, next) => {
+  try {
+    const {email,password} =req.body;
+
+     if (!email  || !password) {
+      const error = new Error("All feilds required");
+      error.statusCode = 400;
+      return next(error);
+    }
+
+    const existingUser = await User.findOne({email});
+
+     if (!existingUser) {
+      const error = new Error("Email not registered");
+      error.statusCode = 401;
+      return next(error);
+    }
+
+    const isVerified = await bcrypt.compare(password, existingUser.password);
+    if(!isVerified){
+        const error = new Error("Password didn't match");
+        error.statusCode = 401;
+        return next(error);
+    }
+
+
+    res.status(200).json({message: 'Login Successfull',data: existingUser});
+
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const UserLogout = async (req, res, next) => {
+  try {
+    res.status(200).json({message:'Logout Successfull'});
+  } catch (error) {
+    next(error);
+  }
+};
